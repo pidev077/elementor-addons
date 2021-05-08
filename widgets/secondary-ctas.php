7bb9e39e0bf91e95876d3252278edb069a217c1f
Vo Vuong <vovanvuong591997@gmail.com>
2021-05-08 09:30:48 +0700

    update element secondary cta

--- widgets/secondary-ctas.php
+++ widgets/secondary-ctas.php
@@ -255,7 +255,7 @@ class Secondary_CTAs extends Widget_Base {
                         ],
                     ],
                     'selectors' => [
-                        '{{WRAPPER}} .secondary-ctas-elements .list-items .item .thumbnail img' => 'width: {{SIZE}}{{UNIT}}',
+                        '{{WRAPPER}} .secondary-ctas-elements .list-items .item .thumbnail img' => 'height: {{SIZE}}{{UNIT}}',
                     ],
                 ]
             );
@@ -273,7 +273,7 @@ class Secondary_CTAs extends Widget_Base {
                         ],
                     ],
                     'selectors' => [
-                        '{{WRAPPER}} .secondary-ctas-elements .list-items .item .thumbnail img' => 'max-width: {{SIZE}}{{UNIT}}',
+                        '{{WRAPPER}} .secondary-ctas-elements .list-items .item .thumbnail img' => 'max-height: {{SIZE}}{{UNIT}}',
                     ],
                 ]
             );
